e9b62c80b35fbb1bec1e9fad0f290e23cc718ddf
Burhan <Burhan@DESKTOP-9NRFKA1>
2022-07-06 15:34:35 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Xps.Packaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfAppBlazor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+         //   Resources.Add("services", serviceCollection.AddScoped<IPrintingService, PrintingService>());
+
+            
+        }
+
+      
+
+        //private void InvokePrint_(object sender, TextChangedEventArgs e)
+        //{
+        //    // Create the print dialog object and set options
+        //    //PrintDialog pDialog = new PrintDialog();
+        //    //pDialog.PageRangeSelection = PageRangeSelection.AllPages;
+        //    ////  pDialog.PrintableAreaHeight=
+        //    //pDialog.UserPageRangeEnabled = true;
+
+        //    //// Display the dialog. This returns true if the user presses the Print button.
+        //    //Nullable<Boolean> print = pDialog.ShowDialog();
+        //    //if (print == true)
+        //    //{
+        //    //    XpsDocument xpsDocument = new XpsDocument("D:\\JWT_Task.doxc", FileAccess.ReadWrite);
+        //    //    FixedDocumentSequence fixedDocSeq = xpsDocument.GetFixedDocumentSequence();
+        //    //    pDialog.PrintDocument(fixedDocSeq.DocumentPaginator, "Test print job");
+        //    //}
+        //}
+    }
+}
